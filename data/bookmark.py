a7e09138568f117540701abe2b925e5753ce110c
Timotimyp <timoxa47474747@gmail.com>
2022-04-30 12:03:49 +1000

    qa

--- data/bookmark.py
+++ data/bookmark.py
@@ -1,8 +1,10 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
 
 
-class Bookmarks(SqlAlchemyBase):
+
+class Bookmarks(SqlAlchemyBase, UserMixin):
     __tablename__ = 'bookmarks'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
--- loginform.py
+++ loginform.py
@@ -1,17 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from flask import redirect, Flask, render_template, request, url_for, make_response
-from data import db_session
+from flask import redirect, Flask, render_template, request, url_for, make_response, logging
+from data import db_session, news_api
 from data.profils import Profile
 from data.search_history import History
 from data.bookmark import Bookmarks
-import requests
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import login_required, LoginManager, login_user, logout_user
 db_session.global_init("db/users.db")
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
@@ -27,6 +29,18 @@ class Buttons(FlaskForm):
     submit = SubmitField("Закладки")
     submit1 = SubmitField("История")
     submit2 = SubmitField("Ссылка")
+    submit3 = SubmitField("Выйти")
+
+
+class Back(FlaskForm):
+    submit = SubmitField("Назад")
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(Profile).get(user_id)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,70 +49,36 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        if check_profils(form.username.data, form.password.data):
+        db_sess = db_session.create_session()
+        user = db_sess.query(Profile).filter(Profile.login == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
             return redirect(f'/success/{form.username.data}')
         else:
             return render_template('login.html',
                                    message="Неправильный логин или пароль",
                                    form=form)
     return render_template("login.html", form=form)
-    # global message
-    # message = " "
-    # if request.method == 'GET':
-    #     return f"""<!DOCTYPE html>
-    # <!-- Coding By CodingNepal - youtube.com/codingnepal -->
-    # <html lang="en" dir="ltr">
-    #   <head>
-    #     <meta charset="utf-8">
-    #     <title>Animated Login Form | CodingNepal</title>
-    #     <link rel="stylesheet" href="{url_for('static', filename='css/2.css')}">
-    #   </head>
-    #   <body>
-    #     <div class="center">
-    #       <h1>Login</h1>
-    #       <form method="post">
-    #         <div class="txt_field">
-    #           <input type="text" name="text" required >
-    #           <span></span>
-    #           <label>Username</label>
-    #         </div>
-    #         <div class="txt_field">
-    #           <input type="password" name="password" required >
-    #           <span></span>
-    #           <label>Password</label>
-    #         </div>
-    #         <div class="pass">Forgot Password?</div>
-    #         <input type="submit" value="Login">
-    #         <div>{url_for(message)}</div>
-    #       </form>
-    #     </div>
-    #
-    #   </body>
-    # </html>"""
-    # elif request.method == 'POST':
-    #     if request.form["text"] == "tt" and request.form["password"]:
-    #         return redirect('/success')
-    #     else:
-    #         global message
-    #         message = "Неправильный логин или пароль"
 
 
 @app.route('/success/<login>', methods=['GET', 'POST'])
+@login_required
 def success(login):
     form = Buttons()
     if form.is_submitted():
         if form.submit.data:
             return redirect(f"/bookmarks/{login}")
         if form.submit1.data:
-            print(2)
+            return redirect(f"/history/{login}")
         if form.submit2.data:
             return redirect("https://t.me/Project3_3_bot")
-    # # if form.validate_on_submit():
-    # #     return redirect("https://t.me/Project3_3_bot")
+        if form.submit3.data:
+            return redirect("/logout")
     return render_template('button.html', form=form)
 
 
 @app.route("/cookie_test")
+@login_required
 def cookie_test():
     visits_count = int(request.cookies.get("visits_count", 0))
     if visits_count:
@@ -115,6 +95,7 @@ def cookie_test():
 
 
 @app.route('/unsuccess', methods=['GET', 'POST'])
+@login_required
 def unsuccess():
     return render_template('404.html')
 
@@ -129,19 +110,51 @@ def check_profils(login, password):
 
 
 @app.route("/bookmarks/<login>", methods=['GET', 'POST'])
+@login_required
 def bookmarks(login):
-    print(return_bookmarks(login))
-    return f"{return_bookmarks(login)}"
+    form = Back()
+    if form.is_submitted():
+        return redirect(f"/success/{login}")
+    news = return_bookmarks(login)
+    return render_template("bookmarks.html", news=news, form=form)
 
 
 def return_bookmarks(login):
     db_sess = db_session.create_session()
     user = db_sess.query(Profile).filter(Profile.login == login).first()
     chat_id = int(user.chat_id)
-    return [prof.reguest for prof in db_sess.query(Bookmarks).filter(Bookmarks.chat_id == chat_id).all()]
+    return [prof.request for prof in db_sess.query(Bookmarks).filter(Bookmarks.chat_id == chat_id).all()]
 
 
+@app.route("/history/<login>", methods=['GET', 'POST'])
+@login_required
+def history(login):
+    form = Back()
+    if form.is_submitted():
+        return redirect(f"/success/{login}")
+    news = return_bookmarks(login)
+    return render_template("history.html", news=news, form=form)
+
+
+def return_history(login):
+    db_sess = db_session.create_session()
+    user = db_sess.query(Profile).filter(Profile.login == login).first()
+    chat_id = int(user.chat_id)
+    return [prof.request for prof in db_sess.query(History).filter(History.chat_id == chat_id).all()]
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/users.db")
+    app.register_blueprint(news_api.blueprint)
+    app.run(port=5000, host='127.0.0.1')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
